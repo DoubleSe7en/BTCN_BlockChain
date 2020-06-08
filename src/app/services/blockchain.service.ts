62ff03992d0f8d1937a3c44a6d9b5417272ea80f
Cường Nguyễn <nguyencuong1827@gmail.com>
2020-06-08 23:53:16 +0700

    finished

--- src/app/services/blockchain.service.ts
+++ src/app/services/blockchain.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {Blockchain} from 'btcn_blockchain_service/src';
+import { Blockchain } from 'btcn_blockchain_service/src/blockchain';
 import EC from 'elliptic';
 
 @Injectable({
@@ -7,10 +7,48 @@ import EC from 'elliptic';
 })
 export class BlockchainService {
   public blockchainInstance = new Blockchain();
-  public walletKeys = [];
-  constructor() { 
+  public walletKeys: Array<IWalletKey> = [];
+
+  constructor() {
     this.blockchainInstance.difficulty = 1;
-    this.blockchainInstance.minePendingTransactions('my-wallet-adress');
-  
+    this.blockchainInstance.minePendingTransactions('hi');
+    this.generateWalletKeys();
   }
+
+  minePendingTransactions() {
+    this.blockchainInstance.minePendingTransactions(
+      this.walletKeys[0].publicKey
+    );
+  }
+
+  addressIsFromCurrentUser(address) {
+    return address === this.walletKeys[0].publicKey;
+  }
+
+  generateWalletKeys() {
+    const ec = new EC.ec('secp256k1');
+    const key = ec.genKeyPair();
+
+    this.walletKeys.push({
+      keyObj: key,
+      publicKey: key.getPublic('hex'),
+      privateKey: key.getPrivate('hex'),
+    });
+
+    console.log(this.walletKeys);
+  }
+
+  getPendingTransactions() {
+    return this.blockchainInstance.pendingTransactions;
+  }
+
+  addTransaction(tx) {
+    this.blockchainInstance.addTransaction(tx);
+  }
+}
+
+export interface IWalletKey {
+  keyObj: any;
+  publicKey: string;
+  privateKey: string;
 }
